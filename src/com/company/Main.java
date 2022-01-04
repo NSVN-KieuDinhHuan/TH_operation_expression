7fb2b2510c20bd509674c3cde8a90a3011829a56
kieuhuan <kieuhuanbka@gmail.com>
2022-01-04 13:01:58 +0700

    tinh dien tich

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float width;
+    float height;
+    Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter Width:");
+        width= scanner.nextFloat();
+        System.out.println("Enter Height:");
+        height= scanner.nextFloat();
+        float area=width*height;
+        System.out.println("area is:"+area);
+    }
+}
